35198aa50099f3c003bd1ccb0b17f2ff7ba63434
Jiong Rui Tan <jruiballolol@gmail.com>
2025-08-17 20:45:47 +0800

    Add echo

--- src/main/java/MrYapper.java
+++ src/main/java/MrYapper.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MrYapper {
     public static void main(String[] args) {
         int len = 50;
@@ -5,6 +7,22 @@ public class MrYapper {
         String greeting = "Hello! I'm Mr Yapper \n"
                         + "What can I do for you?";
         String goodbye = "Bye. Hope to see you again soon!";  
-        System.out.println(line + "\n" + greeting + "\n" + line + "\n"+ goodbye);
+        System.out.println(line + "\n" + greeting + "\n" + line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.trim().equalsIgnoreCase("bye")) {
+                System.out.println(line + "\n" + goodbye + "\n" + line);
+                break;
+            }
+
+            System.out.println(line + "\n" + input + "\n" + line);
+        }
+
+        scanner.close();
     }
 }
