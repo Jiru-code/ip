886e7a1ab6fda19fcfdcba35c9ddb882cdcf7a7f
Jiong Rui Tan <jruiballolol@gmail.com>
2025-08-17 21:45:27 +0800

    Add mark as done functionality

--- src/main/java/MrYapper.java
+++ src/main/java/MrYapper.java
@@ -10,29 +10,49 @@ public class MrYapper {
         String goodbye = "Bye. Hope to see you again soon!";  
         System.out.println(line + "\n" + greeting + "\n" + line);
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String input;
         
         while (true) {
             input = scanner.nextLine();
+            String[] parts = input.trim().split(" ", 2);
+            String command = parts[0];
 
-            if (input.trim().equalsIgnoreCase("bye")) {
+            if (command.trim().equalsIgnoreCase("bye")) {
                 System.out.println(line + "\n" + goodbye + "\n" + line);
                 break;
-            } else if (input.trim().equalsIgnoreCase("list")) {
+            } else if (command.trim().equalsIgnoreCase("list")) {
                 System.out.println(line);
-                if (list.isEmpty()) {
-                    System.out.println("Empty list.");
+                if (tasks.isEmpty()) {
+                    System.out.println("Empty tasks.");
                 } else {
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i).toString());
                     }
                 }
                 System.out.println(line);
+            } else if (command.trim().equalsIgnoreCase("mark") || command.trim().equalsIgnoreCase("unmark")) {
+                if (parts.length < 2) {
+                    System.out.println("Add in a task number");
+                    continue;
+                }
+                
+                int taskNumber = Integer.parseInt(parts[1]);
+                if (taskNumber > 0 && taskNumber <= tasks.size()) {
+                    Task curr = tasks.get(taskNumber - 1);
+                    if (command.trim().equalsIgnoreCase("mark")) {
+                        System.out.println(line + "\n" + curr.markDone() + "\n" + line);
+                    } else {
+                        System.out.println(line + "\n" + curr.markUndone() + "\n" + line);
+                    }
+                } else {
+                    System.out.println("Put in a task number that is valid!");
+                }
             } else {
-                list.add(input);
+                tasks.add(new Task(input));
                 System.out.println(line + "\n" + "added: " + input + "\n" + line);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,44 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone? "X" : " ");
+    }
+
+    public String markDone() {
+        if (isDone) {
+            return "Task has already been marked done!";
+        } else {
+            this.isDone = true;
+            String beDone = "Nice! I've marked this task as done: \n"
+                            + "  [" + this.getStatusIcon() + "] " + this.getDescription();
+            return beDone;
+        }
+    }
+
+    public String markUndone() {
+        if(isDone) {
+            this.isDone = false;
+            String beUndone = "Ok, I've marked this task as not done yet: \n"
+                             + "  [" + this.getStatusIcon() + "] " + this.getDescription();
+            return beUndone;
+        } else {
+            return "Task has not been marked done!";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
