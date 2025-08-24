74fbf29df5f7851a9326bad06537c5ad71daf581
Jiong Rui Tan <jruiballolol@gmail.com>
2025-08-24 22:41:25 +0800

    Modify event date handling

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -5,52 +5,65 @@ import java.time.format.DateTimeParseException;
 
 public class Event extends Task{
     protected LocalDateTime from;
+    protected String stringFrom;
     protected LocalDateTime to;
+    protected String stringTo;
     private static final DateTimeFormatter INPUT_FORMATTER_WITH_TIME = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
     private static final DateTimeFormatter INPUT_FORMATTER_WITHOUT_TIME = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public Event(String input) throws YapperException {
         super(input.split("/from")[0].trim());
+        String[] parts = input.split("/from")[1].split("/to");
+        this.stringFrom = parts[0].trim();
+        this.stringTo = parts[1].trim();
         try {
-            String[] parts = input.split("/from")[1].split("/to");
-            String fromDateString = parts[0].trim();
-            String toDateString = parts[1].trim();
-
-            if (fromDateString.contains(" ")) {
-                this.from = LocalDateTime.parse(fromDateString, INPUT_FORMATTER_WITH_TIME);
+            if (this.stringFrom.contains(" ")) {
+                this.from = LocalDateTime.parse(this.stringFrom, INPUT_FORMATTER_WITH_TIME);
             } else {
-                this.from = LocalDate.parse(fromDateString, INPUT_FORMATTER_WITHOUT_TIME).atStartOfDay();
+                this.from = LocalDate.parse(this.stringFrom, INPUT_FORMATTER_WITHOUT_TIME).atStartOfDay();
             }
 
-            if (toDateString.contains(" ")) {
-                this.to = LocalDateTime.parse(toDateString, INPUT_FORMATTER_WITH_TIME);
+            if (this.stringTo.contains(" ")) {
+                this.to = LocalDateTime.parse(this.stringTo, INPUT_FORMATTER_WITH_TIME);
             } else {
-                this.to = LocalDate.parse(toDateString, INPUT_FORMATTER_WITHOUT_TIME).atStartOfDay();
+                this.to = LocalDate.parse(this.stringTo, INPUT_FORMATTER_WITHOUT_TIME).atStartOfDay();
             }
 
         } catch (DateTimeParseException | ArrayIndexOutOfBoundsException e) {
-            throw new YapperException("Please provide event in the format description /from [date] /to [date]. Dates can be d/M/yyyy HHmm or yyyy-MM-dd.");
+            this.from = null;
+            this.to = null;
         }
     }
 
     public Event(String description, String fromString, String toString) throws YapperException {
         super(description);
+        this.stringFrom = fromString;
+        this.stringTo = toString;
         try {
             this.from = LocalDateTime.parse(fromString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
             this.to = LocalDateTime.parse(toString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
         } catch (DateTimeParseException e) {
-            throw new YapperException("Error loading event dates from file.");
+            this.from = null;
+            this.to = null;
         }
     }
 
     @Override 
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy, ha")) +
+        if (from == null || to == null) {
+            return "[E]" + super.toString() + " (from: " + this.stringFrom + " to: " + this.stringTo + ")";
+        } else {
+            return "[E]" + super.toString() + " (from: " + this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy, ha")) +
                 " to: " + this.to.format(DateTimeFormatter.ofPattern("MMM dd yyyy, ha")) + ")";
+        }
     }
 
     @Override
     public String toFileString() {
-        return "E" + super.toFileString() + " | " + this.from.toString() + " | " + this.to.toString();
+        if (from == null || to == null) {
+            return "E" + super.toFileString() + " | " + this.stringFrom + " | " + this.stringTo;
+        } else {
+            return "E" + super.toFileString() + " | " + this.from.toString() + " | " + this.to.toString();
+        }
     }
 }
